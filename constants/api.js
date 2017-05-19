5706a6258047440f5c956d095e07f5589d6b584c
kdfcoo <kdfcoo@gmail.com>
2017-05-19 22:46:13 +0800

    add loading screen, change main screen

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,27 @@
-export const fetchMeetups = () =>
-    fetch('http://192.168.2.217:3000/api/meetups')
-        .then(res => res.json());
+import axiso from 'axios';
+
+// export const fetchMeetups = () =>
+//     fetch('http://192.168.2.217:3000/api/meetups')
+//         .then(res => res.json());
+
+axiso.defaults.baseURL = 'http://192.168.2.217:3000/api';
+
+const fakeGroupId = '591e5aa961be1d1840ce8498';
+
+class MeetupApi {
+  constructor() {
+    this.groupId = fakeGroupId;
+    this.path = `/groups/${this.groupId}/meetups`;
+  }
+
+  async fetchGroupMeetups() {
+    const { data } = await axiso.get(this.path);
+
+    // console.log(data.meetups);
+    return data.meetups;
+  }
+}
+
+export {
+  MeetupApi,
+};
